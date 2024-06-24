349c0b948a90df8d144a75a134938ef7a508320e
Turalhsynl <tural.huseynli.2005@gmail.com>
2024-06-24 22:30:07 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿class program
+{
+    class Dictionary
+    {
+        public Dictionary<string, string> translate = new Dictionary<string, string>();
+
+        public string this[string word]
+        {
+            get
+            {
+                try
+                {
+                    return translate[word];
+                }
+                catch (KeyNotFoundException)
+                {
+                    return "Word couldn't found";
+                }
+            }
+            set
+            {
+                translate[word] = value;
+            }
+        }
+    }
+    static void Main(string[] args)
+    {
+        Dictionary dic = new Dictionary();
+
+        dic["adam"] = "people";
+
+        Console.WriteLine($"adam: {dic["adam"]}");
+    }
+}
